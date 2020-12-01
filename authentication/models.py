9dc21f62ada1afa1b4b3e96393c17d892e603b4c
Talha Ibne Imam <cse031sust02.freelancer@gmail.com>
2020-12-01 10:34:04 +0600

    basic database model setup

--- authentication/models.py
+++ authentication/models.py
@@ -18,3 +18,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Item, Owner, Restaurant, Order
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('fullname', 'user', 'company_name', 'address')
+    search_fields = ['company_name']
+
+    def fullname(self, obj):
+        return "{} {}".format(obj.user.first_name, obj.user.last_name)
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone', 'website')
+    search_fields = ['name', 'phone']
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'restaurant', 'price')
+    search_fields = ['title']
+    list_filter = ['category']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_no', 'restaurant', 'item', 'price',
+                    'status', 'created_at', 'updated_at')
+    search_fields = ['order_no']
+    list_filter = ['status']
+
+
+admin.site.register(Category)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Owner, OwnerAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
